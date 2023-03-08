ea277a124c2696c8a6e5ddb59f036931b17fa971
voltanz <bimatris13@gmail.com>
2023-03-08 08:11:57 +0700

    Pertemuan 3 Praktikum 1

--- resources/views/product2.blade.php
+++ resources/views/product2.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-<h1 class="text-center">PRODUK 2</h1>
+<h1 class="text-center">Product 2</h1>
 <h6 class="text-center" style="margin-bottom: 50px">Ahmad Bima Tristan Ibrahim</h6>
 <div class="text-center">
     <a href="https://www.tokopedia.com/yamatotools/kalkulator-nasa?extParam=ivf%3Dfalse&src=topads" 
